af5f913111695d85a250b84a35c1cb82ef66d007
ael <ayoub.elmaarouf@arkeup.com>
2025-01-24 10:20:42 +0100

    [update] llm integration safe

--- lua/custom/plugins/llm-integration.lua
+++ lua/custom/plugins/llm-integration.lua
@@ -5,34 +5,27 @@ return {
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
-      -- adding ollama vendor
+      -- provider Ollama
       provider = 'ollama',
       vendors = {
         ollama = {
-          ['local'] = true,
-          endpoint = '127.0.0.1:11434/v1',
-          -- model = 'llama3.1:latest',
-          model = 'codegemma',
-          parse_curl_args = function(opts, code_opts)
-            return {
-              url = opts.endpoint .. '/chat/completions',
-              headers = {
-                ['Accept'] = 'application/json',
-                ['Content-Type'] = 'application/json',
-              },
-              body = {
-                model = opts.model,
-                messages = require('avante.providers').copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-                max_tokens = 2048,
-                stream = true,
-              },
-            }
-          end,
-          parse_response_data = function(data_stream, event_state, opts)
-            require('avante.providers').openai.parse_response(data_stream, event_state, opts)
-          end,
+          __inherited_from = 'openai',
+          api_key_name = '',
+          endpoint = 'https://europe-west1-arkeup-ai-gen-lab.cloudfunctions.net/use-ollama-multi/v1',
+          model = 'codegemma:7b',
         },
       },
+      -- provider Vertex
+      -- provider = 'vertex',
+      -- vertex = {
+      --   endpoint = 'https://LOCATION-aiplatform.googleapis.com/v1/projects/PROJECT_ID/locations/LOCATION/publishers/google/models',
+      --   api_key_name = 'cmd:gcloud auth print-access-token --quiet',
+      --   model = 'gemini-1.5-flash-002',
+      --   timeout = 30000, -- Timeout in milliseconds
+      --   temperature = 0,
+      --   max_tokens = 4096,
+      --   -- ['local'] = false,
+      -- },     -- ollama vendor
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = 'make',
