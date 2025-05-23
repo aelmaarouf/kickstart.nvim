3f50ce8ba35c67bdf1d7b10f6b25d77c2acc7e23
ael <ayoub.elmaarouf@arkeup.com>
2025-05-23 11:59:45 +0200

    [update] changing theme

--- lua/custom/plugins/color_scheme.lua
+++ lua/custom/plugins/color_scheme.lua
@@ -1,12 +1,26 @@
 return {
+  -- {
+  --   'bluz71/vim-moonfly-colors',
+  --   name = 'moonfly',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.opt.termguicolors = true
+  --     vim.cmd.colorscheme 'moonfly'
+  --   end,
+  -- },
   {
-    'bluz71/vim-moonfly-colors',
-    name = 'moonfly',
+    'rose-pine/neovim',
+    name = 'rose-pine',
     lazy = false,
     priority = 1000,
     config = function()
       vim.opt.termguicolors = true
-      vim.cmd.colorscheme 'moonfly'
+      vim.cmd 'colorscheme rose-pine'
+      -- Set the background color
+      vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000' })
+      -- If you also want to ensure the background of floating windows/popups is black
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = '#000000' })
     end,
   },
 }
